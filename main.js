219d3b5bb962271b39d822a4aae88ca34389a3ec
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-08-11 19:27:13 -0300

    Testeando cambios en repositorio

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const indexRouter = require("./routes/index")
+const indexRouter = require("./routes/index");
 
 app.get("/", (req, res) => {
     res.send("Inicio");
